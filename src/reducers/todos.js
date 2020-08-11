f3a81cbbb20e1cba2c3e5b2cbcd2e7ab2195a23d
nianzhilian <776440949@qq.com>
2020-08-11 11:28:13 +0800

    Update todos.js

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,23 +1,46 @@
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case "ADD":
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false,
-        },
-      ];
-      break;
-    case "TOGGLE_TODO":
-      return state.map((todo) =>
-        todo.id == action.id ? { ...todo, completed: !todo.completed } : todo
-      );
-      break;
-    default:
-      return state;
-      break;
+// const todos = (state = [], action) => {
+//   switch (action.type) {
+//     case "ADD":
+//       return [
+//         ...state,
+//         {
+//           id: action.id,
+//           text: action.text,
+//           completed: false,
+//         },
+//       ];
+//       break;
+//     case "TOGGLE_TODO":
+//       return state.map((todo) =>
+//         todo.id == action.id ? { ...todo, completed: !todo.completed } : todo
+//       );
+//       break;
+//     default:
+//       return state;
+//       break;
+//   }
+// };
+
+function addTodo(state,action){
+  return [...state,{
+    id:action.id,
+    text:action.text,
+    completed:false
+  }]
+}
+
+function createReducer(initState,handle){
+  return function reducer(state = initState,action){
+    if(handle.hasOwnProperty(action.type)){
+      return handle[action.type](state,action)
+    }else{
+      return state
+    }
   }
-};
+}
+
+const todos = createReducer([],{
+  'ADD':addTodo
+})
+
 export default todos
