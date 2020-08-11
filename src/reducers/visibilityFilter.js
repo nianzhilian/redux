0ec3b6d85d751dc6a649d77ca620b79c193e8747
nianzhilian <776440949@qq.com>
2020-08-11 11:29:40 +0800

    封装reducers创建函数

--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,12 +1,28 @@
-const visibilityFilter = (state='SHOW_ALL',action)=>{
-    switch (action.type) {
-        case 'SET_VISIBILITY_FILTER':
-            return action.filter
-            break;
+// const visibilityFilter = (state='SHOW_ALL',action)=>{
+//     switch (action.type) {
+//         case 'SET_VISIBILITY_FILTER':
+//             return action.filter
+//             break;
     
-        default:
+//         default:
+//             return state
+//             break;
+//     }
+// }
+function setVisibility(state,action){
+        return action.filter
+}
+//reducers 生成函数
+function createReducers(ininState,handlers){
+    return function reducer(state =ininState,action ){
+        if(handlers.hasOwnProperty(action.type)){
+            return handlers[action.type](state,action)
+        }else{
             return state
-            break;
+        }
     }
 }
+const visibilityFilter = createReducers('SHOW_ALL',{
+    'SET_VISIBILITY_FILTER':setVisibility
+})
 export default visibilityFilter
